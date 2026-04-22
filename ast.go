35bf8cc9deaee24b73037a5b1b7c3c99c475c28d
AshesOfEther <15670490+AshesOfEther@users.noreply.github.com>
2026-04-22 14:23:45 +0200

    abstract syntax tree

---
+++ ast.go
@@ -0,0 +1,130 @@
+package main
+
+type AstNode struct {}
+
+type Statement struct {
+	AstNode
+}
+
+type If struct {
+	Statement
+	condition Expression
+	body []Statement
+	else_ Statement
+}
+
+type While struct {
+	Statement
+	condition Expression
+	body []Statement
+}
+
+type Apply struct {
+	Statement
+	subject Expression
+	body []Statement
+}
+
+type FunctionDecl struct {
+	Statement
+	name string
+	positionalArgs []string
+	namedArgs []NamedArg
+}
+
+type NamedArg struct {
+	Statement
+	name string
+	defaultValue Expression
+}
+
+type Return struct {
+	Statement
+	value *Expression
+}
+
+type Assignment struct {
+	Statement
+	destination Member
+	value Expression
+}
+
+type ExpressionStatement struct {
+	Statement
+	expression Expression
+}
+
+type Expression struct {}
+
+type BinaryOp struct {
+	Expression
+	left Expression
+	right Expression
+	operator Operator
+}
+
+type Operator int
+
+const (
+	Equal Operator = iota
+	NotEqual
+	LessThan
+	GreaterThan
+	LessOrEqual
+	GreaterOrEqual
+	Range
+	RangeInclusive
+	Add
+	Subtract
+	Multiply
+	Divide
+	Modulo
+	And
+	Or
+)
+
+type FunctionCall struct {
+	Expression
+	name string
+	positionalArgs []Expression
+	namedArgs []NamedArg
+	body []Statement
+}
+
+type List struct {
+	elements []Expression
+}
+
+type Literal[T any] struct {
+	Expression
+	Value T
+}
+
+type FloatLiteral Literal[float64]
+type IntLiteral Literal[int64]
+type BooleanLiteral Literal[bool]
+type StringLiteral Literal[string]
+
+type MemberExpression struct {
+	Expression
+	member Member
+}
+
+type Member struct {}
+
+type Name struct {
+	Member
+	name string
+}
+
+type Property struct {
+	Member
+	object Expression
+	property string
+}
+
+type Index struct {
+	Member
+	list Expression
+	index Expression
+}
