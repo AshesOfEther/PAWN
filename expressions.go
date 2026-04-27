9cd31cee5bd77d7410db2f6ac67a4a712f9277f6
Jonas Stæhr Pedersen <GQ08WF@student.aau.dk>
2026-04-27 15:59:48 +0200

    made a func add that checks the diffent types but needs panic

---
+++ expressions.go
@@ -0,0 +1,29 @@
+package main
+
+import ()
+
+func add(Value1 PawnValue, Value2 PawnValue) PawnValue {
+	switch v1 := Value1.(type) {
+	case PawnInt:
+		switch v2 := Value2.(type) {
+		case PawnInt:
+			return PawnInt{v1.v + v2.v}
+		case PawnFloat:
+			return PawnFloat{float64(v1.v) + v2.v}
+		default:
+			return
+		}
+
+	case PawnFloat:
+		switch v2 := Value2.(type) {
+		case PawnInt:
+			return PawnFloat{v1.v + float64(v2.v)}
+		case PawnFloat:
+			return PawnFloat{v1.v + v2.v}
+		default:
+			return
+		}
+	default:
+		return
+	}
+}
