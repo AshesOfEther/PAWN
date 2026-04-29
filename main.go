a5f908d1e28b8209f936f12ee373ced20edf82d0
AshesOfEther <15670490+AshesOfEther@users.noreply.github.com>
2026-04-29 13:13:35 +0200

    main: repl

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 
@@ -11,13 +12,45 @@ import (
 )
 
 func main() {
-	filePath := os.Args[1]
+	repl()
+}
 
-	input, _ := antlr.NewFileStream(filePath)
-	lexer := parsing.NewBoardGameLangLexer(input)
-	stream := antlr.NewCommonTokenStream(lexer, 0)
-	parser := parsing.NewBoardGameLangParser(stream)
-	tree := parser.StatementList()
+func repl() {
+	reader := bufio.NewReader(os.Stdin)
+	environment := newEnvironment(nil)
 
-	fmt.Printf("%#v\n", ast.GenerateStatements(tree.AllStatement()))
+	for true {
+		fmt.Print("> ")
+		source, _ := reader.ReadString('\n')
+
+		input := antlr.NewInputStream(source)
+		lexer := parsing.NewBoardGameLangLexer(input)
+		stream := antlr.NewCommonTokenStream(lexer, 0)
+		parser := parsing.NewBoardGameLangParser(stream)
+		parseTree := parser.StatementList()
+		tree := ast.GenerateStatements(parseTree.AllStatement())
+
+		execute(tree, environment)
+	}
+}
+
+func execute(tree []ast.Statement, environment Environment) {
+	defer func() {
+		if r := recover(); r != nil {
+			if pawnError, ok := r.(PawnError); ok {
+				fmt.Printf("ERROR: %s\n", pawnError.message)
+			} else {
+				panic(r)
+			}
+		}
+	}()
+
+	for _, statement := range tree {
+		if expressionStatement, ok := statement.(ast.ExpressionStatement); ok {
+			result := evaluateExpression(expressionStatement.Expression, environment)
+			fmt.Println(result)
+		} else {
+			evaluateStatement(statement, environment)
+		}
+	}
 }
