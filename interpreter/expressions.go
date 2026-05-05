4158d731089147018818005544c160f601ccc554
Brian-ED <brianellingsgaard9@gmail.com>
2026-05-05 13:06:41 +0200

    Finished primitive func and list comparisons

--- interpreter/expressions.go
+++ interpreter/expressions.go
@@ -2,7 +2,9 @@ package interpreter
 
 import (
 	"fmt"
-	
+	"reflect"
+	"unsafe"
+
 	"pawn/ast"
 )
 
@@ -97,11 +99,21 @@ func evaluateEqual(leftValue PawnValue, rightValue PawnValue) PawnValue {
 		}
 	case PawnList:
 		if right, ok := rightValue.(PawnList); ok {
-			_ = right // TODO
+			result = left.v == right.v
 		}
 	case PawnFunctionPrimitive:
 		if right, ok := rightValue.(PawnFunctionPrimitive); ok {
-			_ = right // TODO
+			result = reflect.ValueOf(left.f).Pointer() == reflect.ValueOf(right.f).Pointer()
+
+			if result {
+				if left.positionalArgCount != right.positionalArgCount {
+					panic("Primitive functions were equal, but had different argument count")
+				}
+				if unsafe.SliceData(left.namedArguments) != unsafe.SliceData(right.namedArguments) {
+					panic("Primitive functions were equal, but had different arguments")
+				}
+			}
+
 		}
 	case PawnFunctionUser:
 		if right, ok := rightValue.(PawnFunctionUser); ok {
