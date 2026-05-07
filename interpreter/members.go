95d14002a5cbf185c8c17a4c4689a6a76ba024ca
Brian-ED <brianellingsgaard9@gmail.com>
2026-05-07 16:14:41 +0200

    Implemented member evaluation

--- interpreter/members.go
+++ interpreter/members.go
@@ -2,6 +2,7 @@ package interpreter
 
 import (
 	"fmt"
+	"slices"
 
 	"pawn/ast"
 )
@@ -14,10 +15,19 @@ type MemberReturnedMap struct {
 	mutateMe map[string]PawnValue
 	nameToMutateAt string
 }
-type MemberReturnedList BasicValue[[]*PawnValue]
+
+type MemberReturnedList struct {
+	list []PawnValue
+}
+
+type MemberReturnedIndex struct {
+	list []PawnValue
+	indexToMutateAt int64
+}
 
 func (_ MemberReturnedMap) memberReturnValue() {}
 func (_ MemberReturnedList) memberReturnValue() {}
+func (_ MemberReturnedIndex) memberReturnValue() {}
 
 func EvaluateMember(member ast.Member, environment Environment) MemberReturnValue {
 	switch member := member.(type) {
@@ -31,60 +41,61 @@ func EvaluateMember(member ast.Member, environment Environment) MemberReturnValu
 			panic(PawnError{
 				fmt.Sprint("Variable \"", member.Name, "\" doesn't exist"),
 			})
-//		case ast.Property:
-//			object, ok := EvaluateExpression(member.Object, environment).(PawnObject)
-//			if !ok {
-//				panic(PawnError{
-//					fmt.Sprint("Tried accessing non-existant property: ", member.Property),
-//				})
-//			}
-//			if _, ok := object.v[member.Property]; ok {
-//				return object.v, member.Property
-//			}
-//			panic(PawnError{
-//				fmt.Sprint("Field \"", member.Property, "\" doesn't exist in object"),
-//			})
-//		case ast.Index:
-//			maybeListEvaluated := evaluateMember(member.List, environment)
-//
-//			var maybeList PawnValue
-//			switch maybeListEvaluated := maybeListEvaluated.(type) {
-//				case MemberReturnedMap:
-//					maybeList = maybeListEvaluated.mutateMe[list.nameToMutateAt]
-//				case MemberReturnedList:
-//					maybeList = list.v
-//			}
-//
-//			listPawn, ok := maybeList.(PawnList)
-//			if !ok {
-//				panic(PawnError{
-//					fmt.Sprint("Tried indexing non-list: ", maybeList),
-//				})
-//			}
-//			list := listPawn.v
-//
-//			indices := make([]int64, len(member.Index))
-//
-//			for i, v := range member.Index {
-//				maybeIndex := EvaluateExpression(v, environment)
-//				indexPawn, ok := maybeIndex.(PawnInt)
-//
-//				if !ok {
-//					panic(PawnError{
-//						fmt.Sprint("Tried indexing with non-integer: ", maybeIndex),
-//					})
-//				}
-//
-//				indices[i] = indexPawn.v
-//			}
-//
-//			placesThatCanMutate := make([]*PawnValue, len(indices))
-//
-//			for i, v := range indices {
-//				placesThatCanMutate[i] = &list[v]
-//			}
-//
-//			return 
+		case ast.Property:
+			object, ok := EvaluateExpression(member.Object, environment).(PawnObject)
+			if !ok {
+				panic(PawnError{
+					fmt.Sprint("Tried accessing non-existant property: ", member.Property),
+				})
+			}
+			if _, ok := object.v[member.Property]; ok {
+				return MemberReturnedMap{object.v, member.Property}
+			}
+			panic(PawnError{
+				fmt.Sprint("Field \"", member.Property, "\" doesn't exist in object"),
+			})
+		case ast.Index:
+			maybeList := EvaluateExpression(member.List, environment)
+			listPawn, ok := maybeList.(PawnList)
+			if !ok {
+				panic(PawnError{
+					fmt.Sprint("Tried indexing non-list: ", maybeList),
+				})
+			}
+
+			if len(member.Index) == 1 {
+				maybeIndex := EvaluateExpression(member.Index[0], environment)
+
+				if indexPawn, ok := maybeIndex.(PawnInt); ok {
+					return MemberReturnedIndex{slices.Clone(*listPawn.v), indexPawn.v}
+				} else {
+					panic(PawnError{
+						fmt.Sprint("Tried indexing with non-integer: ", maybeIndex),
+					})
+				}
+			} else {
+				indices := make([]int64, len(member.Index))
+
+				for i, v := range member.Index {
+					maybeIndex := EvaluateExpression(v, environment)
+
+					if indexPawn, ok := maybeIndex.(PawnInt); ok {
+						indices[i] = indexPawn.v
+					} else {
+						panic(PawnError{
+							fmt.Sprint("Tried indexing with non-integer: ", maybeIndex),
+						})
+					}
+				}
+
+				outputList := make([]PawnValue, len(indices))
+
+				for i, v := range indices {
+					outputList[i] = (*listPawn.v)[v]
+				}
+
+				return MemberReturnedList{outputList}
+			}
 
 		default:
 			panic(fmt.Sprint("Unexpected invalid Member: ", member))
