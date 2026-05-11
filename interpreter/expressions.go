61d065c5f2f8071e100ebd463d75b8d82207f281
Brian-ED <brianellingsgaard9@gmail.com>
2026-05-11 14:32:22 +0200

    Moved MeEx further up to help with merge conflicts

--- interpreter/expressions.go
+++ interpreter/expressions.go
@@ -205,6 +205,22 @@ func numberOperation(
 		}
 }
 
+func evaluateMemberExpression(expression ast.MemberExpression, environment Environment) PawnValue {
+	member := EvaluateMember(expression.Member, environment)
+	switch member := member.(type) {
+		case MemberReturnedMap:
+			// It is impossible for the name to not exist since MemberReturnedMap only has a valid object and field
+			return member.mutateMe[member.nameToMutateAt]
+		case MemberReturnedList:
+			return member.list
+		case MemberReturnedIndex:
+			// It is impossible to be out of bounds since MemberReturnedIndex only has a valid list and index
+			return member.list[member.indexToMutateAt]
+		default:
+			panic(fmt.Sprintf("Unexpected invalid Member return value: %t", member))
+	}
+}
+
 func evaluateAnd(leftValue PawnValue, rightValue PawnValue) PawnBoolean {
 	if leftBool, ok := leftValue.(PawnBoolean); ok {
 		if rightBool, ok := rightValue.(PawnBoolean); ok {
@@ -352,20 +368,4 @@ func evaluateList(list ast.List, environment Environment) PawnList {
 	}
 
 	return PawnList{&newList}
-}
-
-func evaluateMemberExpression(expression ast.MemberExpression, environment Environment) PawnValue {
-	member := EvaluateMember(expression.Member, environment)
-	switch member := member.(type) {
-		case MemberReturnedMap:
-			// It is impossible for the name to not exist since MemberReturnedMap only has a valid object and field
-			return member.mutateMe[member.nameToMutateAt]
-		case MemberReturnedList:
-			return member.list
-		case MemberReturnedIndex:
-			// It is impossible to be out of bounds since MemberReturnedIndex only has a valid list and index
-			return member.list[member.indexToMutateAt]
-		default:
-			panic(fmt.Sprintf("Unexpected invalid Member return value: %t", member))
-	}
 }
