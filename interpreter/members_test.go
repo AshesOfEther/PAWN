95b2a0297e3176b66740332c96a0ab4e262608cd
Brian-ED <brianellingsgaard9@gmail.com>
2026-05-11 14:28:23 +0200

    Now erroring non-object access+non-list indexing

--- interpreter/members_test.go
+++ interpreter/members_test.go
@@ -24,6 +24,15 @@ func TestEvaluateMemberIndex(t *testing.T) {
 		PawnError{"Cannot index with the integer 3 because it is not less than the list's size 2"},
 		func () {EvaluateMember(indexOutOfBounds, env)},
 	)
+	assert.PanicsWithValue(t,
+		PawnError{"Cannot index with the integer 3 because it is not less than the list's size 2"},
+		func () {EvaluateMember(indexOutOfBounds, env)},
+	)
+	indexNonList := ast.Index{ast.BooleanLiteral{true}, []ast.Expression{ast.IntLiteral{3}}}
+	assert.PanicsWithValue(t,
+		PawnError{"Tried indexing non-list: {true}"},
+		func () {EvaluateMember(indexNonList, env)},
+	)
 }
 
 func TestEvaluateMemberListIndex(t *testing.T) {
@@ -53,6 +62,13 @@ func TestEvaluateMemberListIndex(t *testing.T) {
 		EvaluateMember(memberIndexListNotFound, env)
 	})
 
+	// Multi-indexing non-list
+	indexNonList := ast.Index{ast.BooleanLiteral{true}, []ast.Expression{ast.IntLiteral{1}, ast.IntLiteral{1}}}
+	assert.PanicsWithValue(t,
+		PawnError{"Tried indexing non-list: {true}"},
+		func () {EvaluateMember(indexNonList, env)},
+	)
+
 	// TODO After implementing call completely, test changing list in second arg while indexing.
 	// It's supposed to index as the last step, so index errors do not happen if the non-modified list is short.
 }
@@ -68,6 +84,12 @@ func TestEvaluateMemberProperty(t *testing.T) {
 	assert.PanicsWithValue(t, PawnError{"Field \"notProperty\" doesn't exist in object"}, func() {
 		EvaluateMember(ast.Property{Object: objectAsVar, Property: "notProperty"}, env)
 	})
+
+	accesssNonObject := ast.Property{ast.BooleanLiteral{true}, "property"}
+	assert.PanicsWithValue(t,
+		PawnError{"Tried accessing property of non-object: {map[]}"},
+		func () {EvaluateMember(accesssNonObject, env)},
+	)
 }
 
 func TestEvaluateMemberEnvironmentName(t *testing.T) {
