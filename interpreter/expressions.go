6ff1282f457338c758f940c37bfeed7e5d48641d
Brian-ED <brianellingsgaard9@gmail.com>
2026-05-11 15:42:27 +0200

    Indent cases in switch

--- interpreter/expressions.go
+++ interpreter/expressions.go
@@ -85,40 +85,40 @@ func evaluateEqual(leftValue PawnValue, rightValue PawnValue) PawnValue {
 	result := false
 
 	switch left := leftValue.(type) {
-	case PawnBoolean:
-		if right, ok := rightValue.(PawnBoolean); ok {
-			result = left.v == right.v
-		}
-	case PawnObject:
-		if right, ok := rightValue.(PawnObject); ok {
-			result = reflect.ValueOf(left) == reflect.ValueOf(right)
-		}
-	case PawnInt:
-		if right, ok := rightValue.(PawnInt); ok {
-			result = left.v == right.v
-		}
-	case PawnFloat:
-		if right, ok := rightValue.(PawnFloat); ok {
-			result = left.v == right.v
-		}
-	case PawnString:
-		if right, ok := rightValue.(PawnString); ok {
-			result = left.v == right.v
-		}
-	case PawnList:
-		if right, ok := rightValue.(PawnList); ok {
-			result = left.v == right.v
-		}
-	case PawnFunctionPrimitive:
-		if right, ok := rightValue.(PawnFunctionPrimitive); ok {
-			result = left == right
-		}
-	case PawnFunctionUser:
-		if right, ok := rightValue.(PawnFunctionUser); ok {
-			result = left == right
-		}
-	default:
-		panic(fmt.Sprint("Unexpected invalid type: ", leftValue))
+		case PawnBoolean:
+			if right, ok := rightValue.(PawnBoolean); ok {
+				result = left.v == right.v
+			}
+		case PawnObject:
+			if right, ok := rightValue.(PawnObject); ok {
+				result = reflect.ValueOf(left) == reflect.ValueOf(right)
+			}
+		case PawnInt:
+			if right, ok := rightValue.(PawnInt); ok {
+				result = left.v == right.v
+			}
+		case PawnFloat:
+			if right, ok := rightValue.(PawnFloat); ok {
+				result = left.v == right.v
+			}
+		case PawnString:
+			if right, ok := rightValue.(PawnString); ok {
+				result = left.v == right.v
+			}
+		case PawnList:
+			if right, ok := rightValue.(PawnList); ok {
+				result = left.v == right.v
+			}
+		case PawnFunctionPrimitive:
+			if right, ok := rightValue.(PawnFunctionPrimitive); ok {
+				result = left == right
+			}
+		case PawnFunctionUser:
+			if right, ok := rightValue.(PawnFunctionUser); ok {
+				result = left == right
+			}
+		default:
+			panic(fmt.Sprint("Unexpected invalid type: ", leftValue))
 	}
 
 	return PawnBoolean{result}
